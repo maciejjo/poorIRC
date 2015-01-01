ddbfa639fd2ab77673d78b4a44c4e7a63e03c609
Maciej Sobkowski <maciejjo@maciejjo.pl>
2015-01-01 23:58:58 +0100

    Added poorIRC server initialization
    A call to function initializing the server, together with needed
    structures.

--- server/src/main.c
+++ server/src/main.c
@@ -5,10 +5,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "poorIRC.h"
+
 int main(int argc, char **argv)
 {
 
-	unsigned short i;
+	struct poorIRC_config *server_config   = NULL;
+	struct poorIRC_server *server_instance = NULL;
+
+	unsigned short i = 0;
+
 
 	printf("Welcome to poorIRC. The shitty IRC rip-off\n");
 	printf("Number of arguments: %d\n", argc - 1);
@@ -19,6 +25,13 @@ int main(int argc, char **argv)
 			printf("Command: %s\n", argv[i]);
 		else
 			printf("Argument %2u: %s\n", i, argv[i]);
+
+	}
+
+	if(poorIRC_init(server_config, server_instance) != 0) {
+
+		fprintf(stderr, "Server initialization failed, exiting...\n");
+		return EXIT_FAILURE;
 
 	}
 
